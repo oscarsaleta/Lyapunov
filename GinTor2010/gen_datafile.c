e66e53874394afc2970b402f290f8721a2c40c90
Oscar Saleta Reig <oscar.saleta@gmail.com>
2016-05-31 09:01:56 +0200

    new datafile, 40k less rows...

--- GinTor2010/gen_datafile.c
+++ GinTor2010/gen_datafile.c
@@ -6,13 +6,22 @@ int main() {
 
     for (k=0;k<20;k++) {
         for (l=0;l<20;l++) {
-            for (m=0;m<20;m++) {
-                for (n=0;n<20;n++) {
+            for (m=0;m<k;m++) {
+                for (n=0;n<l;n++) {
                     if (k+l <= m+n)
                         continue;
+                    if (k==n && k==2 && l==m && l==0)
+                        continue;
+                    if (l==n && l==0)
+                        continue;
+                    if (k-l-1==0) {
+                        fprintf(stdout,"%d,%d,%d,%d,%d\n",count,m,n,k,l);
+                        count++;
+                        continue;
+                        }
                     if (k==m && l==n)
                         continue;
-                    if (k-l-1==0 || m-n-1==0)
+                    if ((k-l-1)*(m-n-1)==0)
                         continue;
                     fprintf(stdout,"%d,%d,%d,%d,%d\n",count,k,l,m,n);
                     count++;
