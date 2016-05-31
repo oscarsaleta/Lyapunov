100400225e1fdc19e2aa61bb6d2e1a41ba8cc308
Oscar Saleta Reig <oscar.saleta@gmail.com>
2016-05-31 10:59:21 +0200

    more changes to datafile

--- GinTor2010/gen_datafile.c
+++ GinTor2010/gen_datafile.c
@@ -8,15 +8,15 @@ int main() {
         for (l=0;l<20;l++) {
             for (m=0;m<k;m++) {
                 for (n=0;n<l;n++) {
+                    if (k+l<2 || n+m<2)
+                        continue;
                     if (k+l <= m+n)
                         continue;
                     if (k==n && k==2 && l==m && l==0)
                         continue;
                     if (l==n && l==0)
                         continue;
-                    if (k==m && l==n)
-                        continue;
-                    if ((k-l-1)*(m-n-1)==0)
+                    if (k==m && l!=n && (k-l-1)!=0)
                         continue;
                     if (k-l-1==0) {
                         fprintf(stdout,"%d,%d,%d,%d,%d\n",count,m,n,k,l);
