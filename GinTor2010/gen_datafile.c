6e9ee08807732c19b1ea77c89432e97cd84f85ff
Oscar Saleta Reig <oscar.saleta@gmail.com>
2016-06-01 13:19:44 +0200

    Added a tracker of centres and other discarded cases in gen_datafile.c

--- GinTor2010/gen_datafile.c
+++ GinTor2010/gen_datafile.c
@@ -8,19 +8,33 @@ int main() {
         for (l=0;l<25;l++) {
             for (m=0;m<25;m++) {
                 for (n=0;n<25;n++) {
-                    if ( (k+l<2 || n+m<2)
-                         || (k+l <= m+n)
-                         || (k==m && l==n)
-                         || (k==n && k==2 && l==m && l==0)
-                         || (l==n && l==0)
-                         || (k==m && l!=n && (k-l-1)!=0) ) {
-                    } else if (k-l-1==0) {
+                    if (k+l<2 || n+m<2) {
+                        fprintf(stderr,"%d,%d,%d,%d - Not valid (R has to be quadratic at least)\n",k,l,m,n);
+                        continue;
+                    } else if (k+l <= m+n) {
+                        fprintf(stderr,"%d,%d,%d,%d - Repeated (k+l <= m+n)\n",k,l,m,n);
+                        continue;
+                    } else if (k==m && l==n) {
+                        fprintf(stderr,"%d,%d,%d,%d - Same monomial twice\n",k,l,m,n);
+                        continue;
+                    } else if (k==n && k==2 && l==m && l==0) {
+                        fprintf(stderr,"%d,%d,%d,%d - Center: (a) quadratic Darboux\n",k,l,m,n);
+                        continue;
+                    } if (l==n && l==0) {
+                        fprintf(stderr,"%d,%d,%d,%d - Center: (b) holomorphic\n",k,l,m,n);
+                        continue;
+                    } else if (k==m && l!=n && (k-l-1)!=0) {
+                        fprintf(stderr,"%d,%d,%d,%d - Center: (d) Hamiltonian or new Darboux\n",k,l,m,n);
+                        continue;
+                    }
+                    if (k-l-1==0) {
+                        fprintf(stderr,"%d,%d,%d,%d - Center: (c) reversible\n",k,l,m,n);
                         fprintf(stdout,"%d,%d,%d,%d,%d\n",count,m,n,k,l);
                         count++;
-                    } else {
-                        fprintf(stdout,"%d,%d,%d,%d,%d\n",count,k,l,m,n);
-                        count++;
+                        continue;
                     }
+                    fprintf(stdout,"%d,%d,%d,%d,%d\n",count,k,l,m,n);
+                    count++;
                 }
             }
         }
