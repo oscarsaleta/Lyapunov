510fcb16bf17a1e22c7aa52d50dea9c50dd24a4d
Oscar Saleta Reig <oscar.saleta@gmail.com>
2016-06-02 13:01:11 +0200

    Data generator for 3 monomials (not finished)

---
+++ 3monomis/gen_datafile.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int k,l;
+    int m,n;
+    int p,q;
+    int tasknum=0;
+
+    for(k=0;k<20;k++) {
+        for(l=0;l<20;l++) {
+            for(m=0;m<20;m++) {
+                for(n=0;n<20;n++) {
+                    for(p=0;p<20;p++) {
+                        for(q=0;q<20;q++) {
+                           if (k+l<=m+n || m+n<=p+q)
+                               continue;
+                           if (l==n && n==q && q==0)
+                               continue;//darboux?
+                           if (k==m && m==p)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    return 0;
+}
