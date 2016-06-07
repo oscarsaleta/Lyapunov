ed9d6aef2c069c6b0bac7141772a1a45babf3812
Oscar Saleta Reig <oscar.saleta@gmail.com>
2016-06-07 10:46:38 +0200

    identified 3 types of centers for 3 monomials

--- 3monomis/gen_datafile.c
+++ 3monomis/gen_datafile.c
@@ -12,11 +12,14 @@ int main() {
                 for(n=0;n<20;n++) {
                     for(p=0;p<20;p++) {
                         for(q=0;q<20;q++) {
-                           if (k+l<=m+n || m+n<=p+q)
+                           if (k+l<=m+n || m+n<=p+q) // per no repetir casos
                                continue;
                            if (l==n && n==q && q==0)
-                               continue;//darboux?
-                           if (k==m && m==p)
+                               continue; //holomorfs
+                           if (k==q && q==m+n && m==n && l==p && p==0)
+                               continue; //darboux?
+                           if (k==m && m==p && l!=n && l!=q && n!=q && k-l-1!=0)
+                               continue; //new darboux?
                         }
                     }
                 }
