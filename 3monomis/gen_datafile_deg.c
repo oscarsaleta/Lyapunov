00fb35109445b8172e967583d5e124394dd720e8
Oscar Saleta Reig <oscar.saleta@gmail.com>
2016-06-10 10:57:07 +0200

    Mes modificacions als fitxers de dades de cada grau

--- 3monomis/gen_datafile_deg.c
+++ 3monomis/gen_datafile_deg.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int max(int a, int b) {
+    return(a>b?a:b);
+}
+
 int main (int argc, char *argv[]) {
     int k,l,m,n,p,q;
     int maxdeg=atoi(argv[1]);
@@ -8,15 +12,17 @@ int main (int argc, char *argv[]) {
 
     for (k=0;k<maxdeg;k++) {
         for (l=0;l<maxdeg;l++) {
-            if (k+l > maxdeg)
+            if (k+l > maxdeg || (k==0 && l==0))
                 continue;
             for (m=0;m<maxdeg;m++) {
                 for (n=0;n<maxdeg;n++) {
-                    if (m+n > maxdeg || (m==l && n==k))
+                    if (m+n > maxdeg || (m==l && n==k) || (m==0 && n==0))
                         continue;
                     for (p=0;p<maxdeg;p++) {
                         for (q=0;q<maxdeg;q++) {
-                            if (p+q > maxdeg || (p==m && q==n) || (p==k && q==l))
+                            if (p+q > maxdeg || (p==m && q==n) || (p==k && q==l) || (p==0 && q==0))
+                                continue;
+                            if (max(max(k+l,m+n),p+q)<maxdeg)
                                 continue;
                             printf("%d,%d,%d,%d,%d,%d,%d\n",count,k,l,m,n,p,q);
                             count++;
