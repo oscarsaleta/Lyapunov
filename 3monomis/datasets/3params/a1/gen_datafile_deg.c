86873bf6fc42c9b12dadb9fec8a863595be8b8f8
Oscar Saleta Reig <oscar.saleta@gmail.com>
2016-06-22 14:31:22 +0200

    Afegits datasets per a{1,2}=0 on eliminem els nous centres

---
+++ 3monomis/datasets/3params/a1/gen_datafile_deg.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int max(int a, int b) {
+    return(a>b?a:b);
+}
+
+int main (int argc, char *argv[]) {
+    int k,l,m,n,p,q;
+    int maxdeg=atoi(argv[1]);
+    int count=0;
+
+    for (k=0;k<=maxdeg;k++) {
+        for (l=0;l<=maxdeg;l++) {
+            if (k+l > maxdeg || (k==0 && l==0) || k+l<2)
+                continue;
+            for (m=0;m<=maxdeg;m++) {
+                for (n=0;n<=maxdeg;n++) {
+                    if (m+n > maxdeg || (m==k && n==l) || (m==0 && n==0) || m+n<2)
+                        continue;
+                    for (p=0;p<=maxdeg;p++) {
+                        for (q=0;q<=maxdeg;q++) {
+                            if (p+q > maxdeg || (p==m && q==n) || (p==k && q==l)
+                                    || (p==0 && q==0) || p+q<2)
+                                continue;
+                            if (max(max(k+l,m+n),p+q)<maxdeg)
+                                continue;
+                            if (k==m && m==p && (k-l-1!=0))
+                                continue;
+                            if (l==n && n==q)
+                                continue;
+                            if (k-l-1==0)
+                                continue;
+                            // eliminar nous centres
+                            if (m-n-1==0 && k==p)
+                                continue;
+                            else
+                                printf("%d,%d,%d,%d,%d,%d,%d,%d\n",count,count,k,l,m,n,p,q);
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
+}
